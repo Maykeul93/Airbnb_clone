dd58e93f7ff83bdc8f41b19217f7fa09ce18aa1a
michael <michael.mittoo@gmail.com>
2023-07-20 10:24:39 +0200

    add react-icon and Search navbar component

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
 
 const Navbar = () => {
     return(
@@ -22,11 +23,12 @@ const Navbar = () => {
                         "
                     >
                         <Logo />
+                        <Search />
                     </div>
                 </Container>
             </div>
         </div>
-    )
+    );
 }
 
 export default Navbar;
